904dcd9d4d58dbda092d2f8d4e9d0f32a63dc784
Carlos Gómez <drcgomez@hotmail.com>
2023-08-06 20:47:38 -0500

    Commit Inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-store';
+  name = 'Carlos';
+  age = 23;
+  image = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Carlos',
+    age: 23,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+}
